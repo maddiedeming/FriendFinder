ff671731922f230d4859fa8b4d40fe13358597f7
Maddie <maddiedeming1@gmail.com>
2018-03-16 08:30:22 -0400

    Need to finish design of home page then deploy

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+// Global Variables
+var friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+        "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
+    },
+    {
+        "name":"Laura",
+        "photo":"https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+        "scores":[ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+    }    
+];
+// Export
+module.exports = friends;
